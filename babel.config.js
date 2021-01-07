024afb7cc2eeb529dd839f557c62400f38020ecd
jmamadeu <jmamadeu2000@gmail.com>
2021-01-08 00:04:46 +0100

    :sparkles: chore: First packages installation and configuration

---
+++ babel.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+        },
+      },
+    ],
+    '@babel/preset-typescript',
+  ],
+  ignore: ['**/*.spec.ts'],
+  plugins: [
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
+  ],
+};
