a34f5ef98f6999f6b010a4bcbd552fb89018864c
jmamadeu <jmamadeu2000@gmail.com>
2021-01-17 19:46:26 +0100

    :package: Packages to load parameters descorators

--- babel.config.js
+++ babel.config.js
@@ -14,5 +14,7 @@ module.exports = {
   plugins: [
     ['@babel/plugin-proposal-decorators', { legacy: true }],
     ['@babel/plugin-proposal-class-properties', { loose: true }],
+    'babel-plugin-parameter-decorator',
+    'babel-plugin-transform-typescript-metadata',
   ],
 };
