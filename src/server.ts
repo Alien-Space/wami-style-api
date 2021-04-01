1ee7fb50f00450d2ebf3d157fdbdb61aa781db2d
João Amadeu <jmamadeu2000@gmail.com>
2021-04-01 10:18:31 +0100

    :zap: chore: Create upload route

--- src/server.ts
+++ src/server.ts
@@ -8,13 +8,33 @@ import { ApolloServer } from 'apollo-server-express';
 import { ProductResolver, UserResolver } from './resolvers';
 import { createDBConnection } from './database';
 import { createAndGetFirstUser } from './entities';
+import multer from 'multer';
+
+import path from 'path';
+import { storage } from './configs/upload';
 
 const port = process.env.PORT || 4000;
 
 async function main() {
   const app = express();
 
+  var upload = multer({ storage });
+
+  // app.use('/files', (req, res) => res.json({ ok: true }));
+
   app.use(cors());
+  app.use(express.json());
+  app.post('/avatar', upload.single('avatar'), function (req, res, next) {
+    // req.file is the `avatar` file
+
+    console.log(req.file, 'FILE');
+
+    res.json(req.file);
+
+    // req.body will hold the text fields, if there were any
+  });
+
+  app.use('/files', express.static(path.join(__dirname, 'tmp', 'uploads')));
 
   await createDBConnection();
 
